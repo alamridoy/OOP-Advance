ffaf50885eb4557361b0fd13c81e0ec6a0e95a0e
Ridoy <you@example.comalamridoy7@gmail.com>
2022-12-11 14:15:23 +0600

    __dict__|dir()

---
+++ 5.constructor_2.py
@@ -0,0 +1,20 @@
+class Dog:
+  def __init__(self,name,color) -> None:
+    self.name = name
+    self.color = color
+    
+  def update_color(self,color):
+    self.color = color
+    
+    
+  def info(self):
+    print(self.color, self.name , 'is smilling')
+    
+d1 = Dog('Bulldog','brown')
+
+d2 = Dog('Puppy','White')
+d1.update_color('Black')
+d1.info()
+d2.info()
+
+print(d1.__dict__)
