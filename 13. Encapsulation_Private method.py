f456d52a0aaf9874fea5cc783d4806f3bc172ca0
Ridoy <you@example.comalamridoy7@gmail.com>
2023-01-25 13:09:55 +0600

    Encapsulation | Private method

---
+++ 13. Encapsulation_Private method.py
@@ -0,0 +1,16 @@
+class Dog:
+  def __init__(self,name,color) -> None:
+    self.name = name
+    self.__color = color
+    
+  def view(self):
+    print(self.name,self.__color)
+    self._abc()
+  
+  def _abc(self):  #private method
+    print("Hello return")
+
+d1 = Dog('Puppy','Black')
+d2 = Dog('Bulldog','Brown')
+
+d1.view()
